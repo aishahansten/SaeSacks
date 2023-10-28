2608754f46031959689ea4f966a1b47af604bef4
엄예진 <linda3534@naver.com>
2023-10-28 15:14:29 +0900

    [Yejin] instar_3 init

---
+++ Instargram_clone_coding/yejineum/Instargram_3/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'posts'
+urlpatterns = [
+    path('', views.index, name='index'), 
+    path('create/', views.create, name='create'),
+    path('<int:post_pk>/delete/', views.delete, name='delete'),
+]
