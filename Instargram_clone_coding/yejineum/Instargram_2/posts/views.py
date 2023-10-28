95cc1348b1258d13a2efd3f62b67291cee77b868
엄예진 <linda3534@naver.com>
2023-10-28 14:41:30 +0900

    [Yejin] post.html fix & delete views fix

--- Instargram_clone_coding/yejineum/Instargram_2/posts/views.py
+++ Instargram_clone_coding/yejineum/Instargram_2/posts/views.py
@@ -26,6 +26,6 @@ def create(request):
     return render(request, 'posts/create.html', context)
 
 def delete(request, post_pk):
-    post = Post.objects.get(pk=post_pk):
+    post = Post.objects.get(pk=post_pk)
     post.delete()
     return redirect('posts:index')
