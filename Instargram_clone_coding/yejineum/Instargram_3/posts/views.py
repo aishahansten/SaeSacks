3adfd5fb8b318895ed23bc7ffb0bf148174c348e
YeJinEum <yjeum03@gmail.com>
2023-10-30 19:23:51 +0900

    [Yejin] create views fix -> request.FIELS add

--- Instargram_clone_coding/yejineum/Instargram_3/posts/views.py
+++ Instargram_clone_coding/yejineum/Instargram_3/posts/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
+
+
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-    context = {
-        'posts' : posts
-    }
-    return render(request, 'posts/index.html', context)
+    context = {"posts": posts}
+    return render(request, "posts/index.html", context)
+
 
 def create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save()
-            return redirect('posts:index')
+            return redirect("posts:index")
     else:
         form = PostForm()
     context = {
-        'form' : form,
+        "form": form,
     }
-    return render(request, 'posts/create.html', context)
+    return render(request, "posts/create.html", context)
+
 
 def delete(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     post.delete()
-    return redirect('posts:index')
+    return redirect("posts:index")
