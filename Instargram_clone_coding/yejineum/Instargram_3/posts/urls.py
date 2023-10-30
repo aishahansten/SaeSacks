5cf28c4dc79faf28bc623ab2d8bbe88589e0e9fb
YeJinEum <yjeum03@gmail.com>
2023-10-30 20:19:38 +0900

    [Yejin] update url add

--- Instargram_clone_coding/yejineum/Instargram_3/posts/urls.py
+++ Instargram_clone_coding/yejineum/Instargram_3/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'posts'
+app_name = "posts"
 urlpatterns = [
-    path('', views.index, name='index'), 
-    path('create/', views.create, name='create'),
-    path('<int:post_pk>/delete/', views.delete, name='delete'),
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("<int:post_pk>/delete/", views.delete, name="delete"),
+    path("<int:post_pk>/update", views.update, name="update"),
 ]
