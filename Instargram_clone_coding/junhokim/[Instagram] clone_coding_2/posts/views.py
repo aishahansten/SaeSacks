bf0b27623334c8838f641461beb020dcc13b6a22
JUNHO <k3349312@gmail.com>
2023-10-30 23:04:30 +0900

    [JUNHO]Instargram_clone_coding_2

---
+++ Instargram_clone_coding/junhokim/[Instagram] clone_coding_2/posts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request, 'posts/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'posts/create.html', context)
+
+def delete(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    post.delete()
+    return redirect("posts:index")    
