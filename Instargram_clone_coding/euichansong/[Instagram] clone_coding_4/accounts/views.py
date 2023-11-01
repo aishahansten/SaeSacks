cda9ebf7b3777854fc405b60de7905f1c1d3012d
euichan <zxcasdqwer38@gmail.com>
2023-11-02 02:39:00 +0900

    [EuiChan] ICC 4 def login,logout,signup

--- Instargram_clone_coding/euichansong/[Instagram] clone_coding_4/accounts/views.py
+++ Instargram_clone_coding/euichansong/[Instagram] clone_coding_4/accounts/views.py
@@ -8,9 +8,11 @@ from django.contrib.auth import logout as auth_logout
 # Create your views here.
 def login(request):
     if request.method=="POST":
+        # 로그인 인증에 사용할 데이터 입력받는 폼
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
+            return redirect('posts:index')
     else:
         form = AuthenticationForm()
     context = {
@@ -19,10 +21,20 @@ def login(request):
     return render(request, 'accounts/login.html', context)
     
 
-
 def logout(request):
-    pass
-
+    auth_logout(request)
+    return redirect('posts:index')
+    
 
 def signup(request):
-    pass
+    if request.mehtod=="POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
